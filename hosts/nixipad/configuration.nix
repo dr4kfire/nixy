61e08970459113cfd333aa7aab9c04fb8bf48e1e
catnip <147838547+dr4kfire@users.noreply.github.com>
2026-03-26 10:05:24 +0100

    WORK PLSSSSSS AAAAAAA

--- hosts/nixipad/configuration.nix
+++ hosts/nixipad/configuration.nix
@@ -22,6 +22,11 @@
     ./variables.nix
   ];
 
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2QlZceNZSfVyVb+26QH0xlrqaBu9ZS1Oi8="
+  ];
+
   home-manager.users."${config.var.username}" = import ./home.nix;
 
   # Don't touch this
